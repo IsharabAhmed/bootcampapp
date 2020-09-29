2364a35608c0594a597cc31e216be449cc945636
isharabahmed <isharab150@gmail.com>
2020-09-29 18:36:23 +0500

    made jigar.js

---
+++ src/Jigar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Jigar() {
+    return (
+        <h1><i>this lyric line is from <strong>ADELE</strong></i></h1>
+    )
+}
+
+export default Jigar;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Jigar from './Jigar';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Jigar />
   </React.StrictMode>,
   document.getElementById('root')
 );
