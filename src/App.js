c1a5a2cbbcdea2af6567fa0093ae7e7b8d744049
isharabahmed <isharab150@gmail.com>
2020-09-29 19:16:09 +0500

    jigar file and props etc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Jigar from './Jigar';
 //import logo from './logo.svg';
-//import './App.css';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from the other side!!!</h1>      
+      <h1>Hello from the other side!!!</h1>
+      <Jigar singerName="ADELE" songName='Hello'/>      
     </div>
   );
 }
--- src/Jigar.js
+++ src/Jigar.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-function Jigar() {
+function Jigar(props) {
     return (
-        <h1><i>this lyric line is from <strong>ADELE</strong></i></h1>
+        <div>
+            <h3>
+                <i>
+                    -this lyric line is from <strong>{props.singerName}</strong>
+                </i>
+            </h3>
+            <h3>
+                And the song name is <strong>{props.songName}</strong>
+            </h3>
+        
+        </div>
     )
 }
 
